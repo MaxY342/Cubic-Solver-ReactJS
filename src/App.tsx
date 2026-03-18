8a5475ccf8f16826c42218886ff956ddc48f50a9
MaxY342 <max.yang342@gmail.com>
2026-03-18 05:56:06 -0400

    add split components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { CubicSolver } from './components/CubicSolver.tsx'
+import { CubicEquation } from './components/CubicEquation.tsx'
+import { CubicGraph } from './components/CubicGraph.tsx'
+import { CubicHistory } from './components/CubicHistory.tsx'
+import { CubicInput } from './components/CubicInput.tsx'
+import { CubicTable } from './components/CubicTable.tsx'
 
 export const App = () => {
-  return <CubicSolver />
+  return (
+    <div className="app">
+      <CubicInput />
+      <CubicEquation />
+      <CubicGraph />
+      <CubicTable />
+      <CubicHistory />
+      <CubicSolver />
+    </div>
+  )
 }
--- src/components/CubicHistory.tsx
+++ src/components/CubicHistory.tsx
@@ -1,3 +1,3 @@
 export const CubicHistory = () => {
-    <> </>
+    return <></>
 }
